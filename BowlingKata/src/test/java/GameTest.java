e0a99f3b6ebe1c08cee55861334d3462b0c72777
christophe.godard <christophe.godard@invivoo.com>
2019-04-02 16:37:31 +0200

    [BowlingKata] Add not compiling first test

---
+++ BowlingKata/src/test/java/GameTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class GameTest {
+
+    @Test
+    public void should_return_score_0_when_20_rolls_of_0_pin() {
+        //Given
+        Game game = new Game();
+
+        //When
+        for (int i = 0; i < 20; i++) {
+            game.roll(0);
+        }
+
+        //Then
+        assertThat(game.score()).isEqualTo(0);
+    }
+}
