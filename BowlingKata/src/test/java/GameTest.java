d56e49d53173d6c2081fc7d4cadb183777a31c99
christophe.godard <christophe.godard@invivoo.com>
2019-04-02 16:42:38 +0200

    [BowlingKata] Add a second failed test

--- BowlingKata/src/test/java/GameTest.java
+++ BowlingKata/src/test/java/GameTest.java
@@ -17,4 +17,18 @@ public class GameTest {
         //Then
         assertThat(game.score()).isEqualTo(0);
     }
+
+    @Test
+    public void should_return_score_20_when_20_rolls_of_1_pin() {
+        //Given
+        Game game = new Game();
+
+        //When
+        for (int i = 0; i < 20; i++) {
+            game.roll(1);
+        }
+
+        //Then
+        assertThat(game.score()).isEqualTo(20);
+    }
 }
